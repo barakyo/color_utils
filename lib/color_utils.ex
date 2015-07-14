9fa3758a79128c9a71b8034bdc82f2a4db022098
Barack Karavani <bkarvani@edmunds.com>
2015-07-14 10:56:16 -0700

    Completed dec->hex and rgb->hex

--- lib/color_utils.ex
+++ lib/color_utils.ex
@@ -1,41 +1,45 @@
 defmodule ColorUtils do
 
-  @hex_symbols = %{
-    0 : '0',
-    1 : '1',
-    2 : '2',
-    3 : '3',
-    4 : '4',
-    5 : '6',
-    7 : '7',
-    8 : '8',
-    9 : '9',
-    10 : 'A',
-    11 : 'B',
-    12 : 'C',
-    13 : 'D',
-    14 : 'E',
-    15 : 'F'
+  @hex_symbols %{
+    0 => "0",
+    1 => "1",
+    2 => "2",
+    3 => "3",
+    4 => "4",
+    5 => "6",
+    7 => "7",
+    8 => "8",
+    9 => "9",
+    10 => "A",
+    11 => "B",
+    12 => "C",
+    13 => "D",
+    14 => "E",
+    15 => "F"
   }
 
   defmacro __using__(_) do
 
   end
 
-  # def rgb_to_hex(%RGB{} = rgb) do
-  #   # Comment
-  # end
+  def rgb_to_hex(%RGB{} = rgb) do
+    # get colors as hex
+    blue = decimal_to_hex(rgb.blue)
+    red = decimal_to_hex(rgb.red)
+    green = decimal_to_hex(rgb.green)
+    "#" <> red <> green <> blue
+  end
 
   def decimal_to_binary(num) do
     decimal_to_binary(num, [])
   end
 
   def decimal_to_binary(num, remainders) when num > 0 do
-    decimal_to_binary(div(num, 2), remainders ++ [rem(num, 2)])
+    decimal_to_binary(div(num, 2), [rem(num, 2)] ++ remainders)
   end
 
   def decimal_to_binary(0, remainders) do
-    Enum.reverse(remainders)
+    remainders
   end
 
   def decimal_to_hex(num) do
@@ -43,9 +47,8 @@ defmodule ColorUtils do
   end
 
   def decimal_to_hex(num, hex) when num > 0 do
-    IO.inspect @hex_symbols
     remainder = Map.get(@hex_symbols, rem(num, 16))
-    decimal_to_hex(div(num, 16), hex ++ remainder)
+    decimal_to_hex(div(num, 16), remainder <> hex)
   end
 
   def decimal_to_hex(0, hex) do
--- test/color_utils_test.exs
+++ test/color_utils_test.exs
@@ -2,15 +2,11 @@ defmodule ColorUtilsTest do
   use ExUnit.Case
   use ColorUtils
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "converting from rgb to hex" do
+    rgb = %RGB{red: 200, blue: 200, green: 200}
+    hex = ColorUtils.rgb_to_hex(rgb)
+    assert hex == "#C8C8C8"
   end
-
-  # test "converting from rgb to hex" do
-  #   rgb = %RGB{red: 200, blue: 200, green: 200}
-  #   hex = ColorUtils.rgb_to_hex(rgb)
-  #   assert hex == "#7B8499"
-  # end
 
   test "decimal to binary" do
     binary_list = ColorUtils.decimal_to_binary(2)
@@ -18,12 +14,10 @@ defmodule ColorUtilsTest do
   end
 
   test "decimal to hex" do
-    binary_list = ColorUtils.decimal_to_hex(20)
-    assert binary_list == 14 
+    hex_20 = ColorUtils.decimal_to_hex(20)
+    assert hex_20 == "14"
+    hex_255 = ColorUtils.decimal_to_hex(255)
+    assert hex_255 == "FF"
   end
 
-  # test "conver to the number 5 from decimal to binary" do
-  #   binary_list = ColorUtils.decimal_to_binary(5)
-  #   assert binary_list == [1, 0, 1]
-  # end
 end
