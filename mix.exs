fe20b8bab6bc9d03ad4daf149ecbab5bac6269d2
Barak <itsbarakyo@gmail.com>
2016-01-26 20:25:50 -0800

    Bumped to v0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ColorUtils.Mixfile do
 
   def project do
     [app: :color_utils,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
